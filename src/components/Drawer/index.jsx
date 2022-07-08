1bfdac0eed3fd1e6ced33ad293115a025ab9e2ad
sephael <deadsalamanderninjatroll@gmail.com>
2022-07-08 23:00:01 +0700

    upd

---
+++ src/components/Drawer/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from './Drawer.module.scss';
+
+export default function Drawer() {
+  console.log(styles);
+  return (
+    <div /* style={{ display: 'none' }} */ className={styles.overlay}>
+      <div className={styles.drawer}>
+        <h2 className='mb-30 d-flex justify-between '>
+          Корзина <img className='removeBtn cu-p' src='/img/btn-remove.svg' alt='remove' />
+        </h2>
+        <div className='items'>
+          <div className='cart-item d-flex align-center mb-20'>
+            <div style={{ backgroundImage: 'url(/img/image5.png)' }} className='cartItemImg'></div>
+            <div className='mr-20 flex'>
+              <p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className='removeBtn' src='/img/btn-remove.svg' alt='remove' />
+          </div>
+        </div>
+        <div className='cartTotalBlock'>
+          <ul>
+            <li>
+              <span>Итого:</span>
+              <div></div>
+              <b>21 498 руб.</b>
+            </li>
+            <li>
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>1074 руб.</b>
+            </li>
+          </ul>
+          <button className='greenButton'>
+            Оформить заказ. <img src='/img/arrow.svg' alt='arrow' />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
